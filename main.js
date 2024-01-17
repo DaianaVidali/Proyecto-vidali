9635b48202d9b5dc7470125127835ad7f476d68c
Daiana Vidali <vidalidaiana@gmail.com>
2024-01-17 19:43:22 -0300

    agregamos main.js

--- main.js
+++ main.js
@@ -1,162 +1,67 @@
-/*alert("Bienvenido a nuestra pagina");
+alert("Bienvenido/a a Kosiuko");
 
-let nombre = prompt("Ingrese su nombre");
- 
-let edad = Number (prompt("Ingrese su edad"));
+/*Nombre tutor: Maxi*/
+let nombre = prompt("Ingrese su nombre de usuario");
 
-
-if (edad < 16) {
-    alert("No puedes registrarte, eres menor de edad");
-} else if (edad < 17) {
-    alert("Puedes registrarte con la autorizacion de un adulto");
-
-} else {
-    alert("Puedes registrarte");
-} 
-function agregarMail(Mail){
-    alert(`Agregar mail ${mail}`);
-}
-let mail = prompt("Ingrese su mail si desea recibir las novedades");
-agregarMail
-alert("Gracias por unirte a la comunidad Kosiuko, estaras recibiendo las novedades"); */
-
-/*SEGUNDA PRE ENTREGA 
-alert("Bienvenido Ezequiel");
-
-
-const productos = [
-    { nombre: "1.remera giddy up", precio: 18000 }, 
-    { nombre: "2.sweater believe", precio: 41250 },
-    { nombre: "3.chupin flora", precio: 39000 },
-];
-
-let carrito = [];
-let seleccion = prompt("Hola ¿te gustaria comprar algún producto? si o no");
-
-while (seleccion !== "si" && seleccion !== "no") {
-    alert("Seleccione una opción");
-    seleccion = prompt("Hola ¿te gustaria comprar algún producto? si o no");
-}
-
-if (seleccion === "si") {
-    alert("Nuestra lista de productos ya se encuentra disponible");
-    let losProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
-    alert(losProductos.join(" - "));
-} else if (seleccion === "no") {
-    alert("Gracias por visitar Kosiuko");
-}
-
-while (seleccion === "si") {
-    let producto = prompt("Agregar producto al carrito: remera giddy up, sweater believe, chupin flora");
-    let precio = 0;
-
-    if (producto === "remera giddy up" || producto === "sweater believe" || producto === "chupin flora") {
-        switch (producto) {
-            case "chupin flora":
-                precio = 39000;
-                break;
-            case "sweater believe":
-                precio = 41250;
-                break;
-            case "remera giddy up":
-                precio = 18000;
-                break;
-        }
-        
-         carrito.push({ nombre: producto, precio: precio });
-        
-        alert(producto + " se ha agregado al carrito");
-    } else {
-        alert("Producto no identificado. Intente de nuevo");
-    }
+if (nombre=="maxi"){
+    alert("Hola " +nombre + " esperamos que los productos cargados sean de tu agrado");
     
-    seleccion = prompt("¿Desea agregar otro producto al carrito? si o no");
+}
+else{
+    alert("No eres Maxi");
 }
 
-if (carrito.length > 0) {
-    let total = carrito.reduce((sum, product) => sum + product.precio, 0);
-    alert("Carrito de compras:");
-    carrito.forEach((item) => alert(item.nombre + " - Precio: " + item.precio + "$"));
-    alert("Total a pagar: " + total + "$");
-} else {
-    alert("Gracias por visitar Kosiuko");
-} */
+let opcion = prompt("Elija la opcion que desea \n 1.Remeras \n 2.Sweaters \n 3.Jeans");
 
+switch (opcion) {
+    case "1":
+        alert("Tenemos en stock la remera Stardust y Giddy Up");
+        break;
+    case "2":
+        alert("Tenemos en stock el sweater Believe y My Heart");
+        break;
+    case "3":
+        alert("Tenemos en stock el jean Ginger y Flora");
+        break;
+    default:
+        alert("Elegiste una opcion no valida");
+        break;
 
-  
-function obtenerCarrito() {
-    return JSON.parse(localStorage.getItem('carrito')) || [];
+}
+let remera = prompt("Elija la opcion que desea, si la opcion no es la requerida siga pasando \n 1.Remera Stardust \n 2.Remera Giddy Up");
+
+switch (remera) {
+    case "1":
+        alert("La remera Stardust se agrego a su carrito");
+        break;
+    case "2":
+        alert("La remera Giddy Up se agrego a su carrito");
+        break;
 }
 
-function guardarCarrito(carrito) {
-    localStorage.setItem('carrito', JSON.stringify(carrito));
+let sweater = prompt("Elija la opcion que desea, si la opcion no es la requerida siga pasando \n 1.Sweater Believe \n 2.Sweater My Heart");
+
+switch (sweater) {
+    case "1":
+        alert("El sweater Believe se agrego a su carrito");
+        break;
+    case "2":
+        alert("El sweater My Heart se agrego a su carrito");
+        break;
 }
 
-function agregarAlCarrito(producto, precio) {
-    let carrito = obtenerCarrito();
-    carrito.push({ producto, precio });
-    guardarCarrito(carrito);
-    actualizarCarrito();
+let jean = prompt("Elija la opcion que desea, si la opcion no es la requerida siga pasando \n 1.Jean Ginger  \n 2.Jean Flora");
+
+switch (jean) {
+    case "1":
+        alert("El jean Ginger se agrego a su carrito");
+        break;
+    case "2":
+        alert("El jean Flora se agrego a su carrito");
+        break;
 }
 
-function actualizarCarrito() {
-    let carrito = obtenerCarrito();
-    let listaCarrito = document.getElementById('lista-carrito');
-    let totalCompra = 0;
-
-    
-    listaCarrito.innerHTML = '';
-
-     carrito.forEach(item => {
-        listaCarrito.innerHTML += `<p>${item.producto} - $${item.precio}</p>`;
-        totalCompra += item.precio;
-    });
-
-    document.getElementById('total-compra').innerText = `Total de la compra: $${totalCompra}`;
-
-    document.getElementById('carrito-titulo').innerText = `CARRITO DE COMPRAS (${carrito.length})`;
+const despedirse = (nombre) => {
+    alert("Muchas gracias por tu compra "  + nombre)
 }
-
-function pagar() {
-    let carrito = obtenerCarrito();
-    if (carrito.length === 0) {
-        alert('El carrito está vacío. Agrega productos antes de pagar.');
-        return;
-    }
-
-   
-    let totalCompra = carrito.reduce((total, item) => total + item.precio, 0);
-    alert(`Pago realizado. Total de la compra: $${totalCompra}. Gracias por tu compra.`);
-    
-
-    guardarCarrito([]);
-    actualizarCarrito();
-}
-
-window.onload = actualizarCarrito; 
-
-Swal.fire({
-  title: "KOSIUKO",
-  text: "Siempre es importante amarse y hacernos un mimo a uno mismo ¿Estás liste?",
-  
-}); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
+    despedirse("Maxi");
