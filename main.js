c5b53c8a4a9f82e338635519df634fa280480982
Daiana Vidali <vidalidaiana@gmail.com>
2024-02-05 17:48:10 -0300

    cambio de nombre

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+
 alert("Bienvenido/a a Kosiuko");
 
-/*Nombre tutor: Maxi*/
+/*Nombre tutor: tutor*/
 let nombre = prompt("Ingrese su nombre de usuario");
 
-if (nombre=="maxi"){
+if (nombre=="tutor"){
     alert("Hola " +nombre + " esperamos que los productos cargados sean de tu agrado");
     
 }
 else{
-    alert("No eres Maxi");
+    alert("No eres el tutor");
 }
 
 let opcion = prompt("Elija la opcion que desea \n 1.Remeras \n 2.Sweaters \n 3.Jeans");
@@ -28,40 +29,6 @@ switch (opcion) {
         break;
 
 }
-let remera = prompt("Elija la opcion que desea, si la opcion no es la requerida siga pasando \n 1.Remera Stardust \n 2.Remera Giddy Up");
 
-switch (remera) {
-    case "1":
-        alert("La remera Stardust se agrego a su carrito");
-        break;
-    case "2":
-        alert("La remera Giddy Up se agrego a su carrito");
-        break;
-}
 
-let sweater = prompt("Elija la opcion que desea, si la opcion no es la requerida siga pasando \n 1.Sweater Believe \n 2.Sweater My Heart");
 
-switch (sweater) {
-    case "1":
-        alert("El sweater Believe se agrego a su carrito");
-        break;
-    case "2":
-        alert("El sweater My Heart se agrego a su carrito");
-        break;
-}
-
-let jean = prompt("Elija la opcion que desea, si la opcion no es la requerida siga pasando \n 1.Jean Ginger  \n 2.Jean Flora");
-
-switch (jean) {
-    case "1":
-        alert("El jean Ginger se agrego a su carrito");
-        break;
-    case "2":
-        alert("El jean Flora se agrego a su carrito");
-        break;
-}
-
-const despedirse = (nombre) => {
-    alert("Muchas gracias por tu compra "  + nombre)
-}
-    despedirse("Maxi");
